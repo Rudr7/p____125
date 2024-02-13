dd06cdf501e8099d1825cb7ad0913b22b7cfa0ad
Rudr7 <124049700+Rudr7@users.noreply.github.com>
2024-02-13 16:52:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(750,170);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+
+    poseNet.on('pose', gotPoses);
+   }
+   function draw(){
+    background('#ffffff');
+    document.getElementById("font").innerHTML="width and the height of the text will be "+ difference +"px";
+    fill('#F90093');
+    textSize(difference)
+    text('Rudr', 30, 200 );
+   }
+   function modelLoaded() {
+    console.log('PoseNet is Initialized');
+   }
+   function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX +" rightWristX = "+ rightWristX + " difference = " + difference);
+    }
+   }
